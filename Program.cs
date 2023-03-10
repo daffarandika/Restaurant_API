6e210158f3299f28976247d43ccc7bfbcbd1fc4d
dffrndik <daffa.randika.3@gmail.com>
2023-03-10 15:56:33 +0700

    swaggerUI now has working Auth.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using WebApplication1.Models;
@@ -11,16 +12,44 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(o =>
+//builder.Services.AddSwaggerGen(o =>
+//{
+//    o.AddSecurityDefinition("Oauth", new OpenApiSecurityScheme
+//    {
+//        Description = "JWT Authorization header using the Bearer scheme.\nEnter your token in the text input below.",
+//        In = ParameterLocation.Header,
+//        Name = "Authorization",
+//        Type = SecuritySchemeType.ApiKey
+//    });
+//    o.OperationFilter<SecurityRequirementsOperationFilter>();
+//});
+builder.Services.AddSwaggerGen(c =>
 {
-    o.AddSecurityDefinition("Oauth", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme.\nEnter your token in the text input below.",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    o.OperationFilter<SecurityRequirementsOperationFilter>();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Restaurant Management", Version = "v1" });
+    c.AddSecurityDefinition( "token",
+        new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme.\nEnter your token in the text input below.",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer",
+            In = ParameterLocation.Header,
+            Name = HeaderNames.Authorization
+        }
+    );
+    c.AddSecurityRequirement( new OpenApiSecurityRequirement {
+            {
+                new OpenApiSecurityScheme {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "token"
+                    },
+                },
+                Array.Empty<string>()
+            }
+        }
+    );
 });
 builder.Services.AddDbContext<DbRestaurantContext>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
